763ba62ae15e9d652f3127eb9fc7f9563d0d689e
LorraneAlkimim <lorranealkmim@yahoo.com.br>
2020-07-04 10:09:22 -0300

    repository listing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { useState } from 'react';
-import { FiSearch } from 'react-icons/fi';
+import { FiSearch, FiGitPullRequest } from 'react-icons/fi';
+import {
+  FaExclamationCircle,
+  FaCodeBranch,
+  FaCircle,
+  FaStar
+} from 'react-icons/fa';
+
+import Figure from './assets/figure.svg';
 
 import './App.css';
-import Figure from './assets/figure.svg';
-import Repository from './components/Repository';
+import './repositories.css';
+
+import api from './services/api';
+
 
 function App() {
-  const [search, setSearch] = useState('sad');
+  const [search, setSearch] = useState();
+  const [repos, setRepos] = useState();
+
+  async function handleSearch(){
+    const response = await api.get(`${search}/repos`);
+    setRepos(response.data);
+  }
 
   return (
     <>
@@ -16,12 +32,12 @@ function App() {
             type="search"
             name="search"
             placeholder="Procurar..."
-            value={search}
+            
             onChange={event => setSearch(event.target.value)}
           />
           <div
             className="search-button"
-            onClick={() => { alert('Vrau') }}
+            onClick={() => handleSearch()}
           >
             <FiSearch
               color="#eee"
@@ -32,12 +48,41 @@ function App() {
       </header>
 
       <main>
-        {search ?
+        {repos ?
           <>
-            <p className="username">Repositórios de <b>@username</b></p>
-            <Repository />
-            <Repository />
-            <Repository />
+            <p className="username">Repositórios de <b>{search}</b></p>
+            {repos.map(repo => (
+              <div key={repo.id} className="repo-container">
+                <h1>{repo.name}</h1>
+                <p>{repo.description}</p>
+          
+                <div className="repo-infos">
+                  <div>
+                    <FaCircle size={16} color="#767D9F" />
+                    <p>{repo.language}</p>
+                  </div>
+                  <div className="link">
+                    <FaCodeBranch size={16} color="#767D9F" />
+                    <p>{repo.forks_count}</p>
+                  </div>
+                  <div className="link">
+                    <FaStar size={16} color="#767D9F" />
+                    <p>{repo.stargazers_count}</p>
+                  </div>
+                  <div className="link">
+                    <FaExclamationCircle size={16} color="#767D9F" />
+                    <p>{repo.open_issues}</p>
+                  </div>
+                  <div className="link">
+                    <FiGitPullRequest size={16} color="#767D9F" />
+                    <p>{repo.pulls}</p>
+                  </div>
+                  <div>
+                    <p>Atualizado em {repo.updated_at}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </>
         : 
           <div className="initial-screen">
